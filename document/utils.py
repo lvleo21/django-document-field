7cd9330a1e2240302b49b4e3a08439d5b0b96a52
Leonardo Veras <verasleonardo210@gmail.com>
2025-04-24 13:54:04 -0300

    ♻️ refactor: replace magic numbers with constants for CPF and CNPJ lengths in format_document function

--- document/utils.py
+++ document/utils.py
@@ -8,8 +8,8 @@ def format_document(value: str) -> str:
     if not value:
         return ""
     value = value.strip()
-    if len(value) == 11:
+    if len(value) == CPF_LENGTH:
         return f"{value[:3]}.{value[3:6]}.{value[6:9]}-{value[9:]}"
-    elif len(value) == 14:
+    elif len(value) == CNPJ_LENGTH:
         return f"{value[:2]}.{value[2:5]}.{value[5:8]}/{value[8:12]}-{value[12:]}"
     return value
